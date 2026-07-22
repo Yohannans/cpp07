ac4fe5685e5bb42115edc86123d12362bf40bc6b
yansquer <yansquer@student.42.fr>
2026-07-22 17:33:23 +0200

    ex01 : added test for array of strings

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,6 +6,11 @@ void printValue(int &value)
 	std::cout << value << " ";
 }
 
+void printString(std::string &str)
+{
+	std::cout << str << " " << std::endl;
+}
+
 int main()
 {
 	int array[] = {1, 2, 3, 4, 5};
@@ -14,6 +19,11 @@ int main()
 	std::cout << "Array elements: ";
 	iter(array, length, printValue);
 	std::cout << std::endl;
+
+	std::string strArray[] = {"Hello", "World", "!"};
+	size_t strLength = sizeof(strArray) / sizeof(strArray[0]);
+	std::cout << "String array elements: ";
+	iter(strArray, strLength, printString);
 
 	return 0;
 
